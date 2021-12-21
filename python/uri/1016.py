5d8468edd3bfdeb5ecd6989fa7af9f47ee71fb5a
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-12-21 02:03:00 -0300

    uri 1016 v2

--- python/uri/1016.py
+++ python/uri/1016.py
@@ -1,4 +1,10 @@
-distance = int(input())
-time = distance * 2 
+vm1 = 60
+vm2 = 90 
 
-print(f'{time} minutos')
+vm3_km_h = abs(vm1 - vm2)
+vm3_km_min = vm3_km_h / 60 
+
+distance = int(input())
+time_min = distance / vm3_km_min
+
+print(f'{int(time_min)} minutos')
